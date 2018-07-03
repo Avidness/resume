41b50e2bc62dc4467f32e2c6326d9fdfd2097ed2
Avidness <adavidness@gmail.com>
2018-07-02 20:24:55 -0500

    Clean up the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,32 @@
 import React from 'react'
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faPhone } from '@fortawesome/fontawesome-free-solid'
+import { faPhone, faEnvelope, faFilePdf, faCodeBranch, faCode, faCaretDown } from '@fortawesome/fontawesome-free-solid'
+import { faGithub, faGithubAlt } from '@fortawesome/fontawesome-free-brands'
+import imgProfile from '../img/me.png'
+import '../styles/header.css';
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   
-  <div className="svg-bg">
-    <br />
-    <br />
-    <Typography variant="display2" color="inherit" align="center">
-      <img className="circle" src="https://placekitten.com/200/200" alt="profile" style={{height:200}} />
-    </Typography>
-    <Typography variant="display2" color="inherit" align="center">
-      Alan's resume
-    </Typography>
-    <Typography variant="subheading" color="inherit" align="center">
-      Full-Stack Web Developer
-    </Typography>
-    <br />
-    <br />
-    <br />
-    <Typography variant="button" color="inherit" align="center">
-      <Button variant="fab" color="inherit" aria-label="Call">
-        <FontAwesomeIcon icon={faPhone} />
-      </Button>
-    </Typography>
-    <Typography variant="title" color="inherit" align="center">
-      Call Now
-    </Typography>
-    <Typography variant="title" color="inherit" align="center">
-      123-123-1234
-    </Typography>
-    <br />
-    <br />
-    <br />
-    <Typography variant="headline" color="inherit" align="center">
-      More info
-    </Typography>
-    <Typography variant="title" color="inherit" align="center">
-      <Button color="inherit">
-        <i className="fa fa-2x fa-chevron-down" />
-      </Button>
-    </Typography>
+  <div id="header">
+
+    <img id="profile_image" className="center" src={imgProfile} alt="profile" />
+    <span id="title" className="center">Alan David Ness</span>
+    <span id="subtext" className="center">Full-Stack Web Developer</span>
+
+    <div id="header_icons" className="center">
+      <FontAwesomeIcon icon={faPhone} size="2x" />
+      <FontAwesomeIcon icon={faGithub} size="2x" />
+      <FontAwesomeIcon icon={faFilePdf} size="2x" />
+      <FontAwesomeIcon icon={faCodeBranch} size="2x" />
+      <FontAwesomeIcon icon={faEnvelope} size="2x" />
+      <FontAwesomeIcon icon={faCode} size="2x" />
+      <FontAwesomeIcon icon={faGithubAlt} size="2x" />
+    </div>
+    
+    <div id="header_bottom" className="center">
+      <p>More info</p>
+      <FontAwesomeIcon icon={faCaretDown} size="3x" />
+    </div>
   </div>
 )
 
