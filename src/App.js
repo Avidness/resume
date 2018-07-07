9ccec5bb4130e9620d7efefaa07caa82c5b38be9
Avidness <adavidness@gmail.com>
2018-07-07 18:56:17 -0500

    Add customized Material UI theme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import Header from './components/Header'
 import Steps from './components/Steps'
 import './styles/App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      'Press Start 2P',
+    ].join(','),
+  },
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider theme={theme}>
         <Header />
         <Steps />
         <p className="App-intro parallax">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
