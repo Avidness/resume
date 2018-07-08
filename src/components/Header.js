4a51a0a84ce5d3cefa652f85f55e154b72536468
Avidness <adavidness@gmail.com>
2018-07-08 00:55:56 -0500

    Adjust icon styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faPhone, faEnvelope, faFilePdf, faCodeBranch, faCode, faCaretDown } from '@fortawesome/fontawesome-free-solid'
-import { faGithub, faGithubAlt } from '@fortawesome/fontawesome-free-brands'
+import { faPhone, faEnvelope, faFilePdf, faCaretDown } from '@fortawesome/fontawesome-free-solid'
+import { faGithub, faSlack } from '@fortawesome/fontawesome-free-brands'
 import imgProfile from '../img/me.png'
 import '../styles/header.css';
 
@@ -10,18 +10,19 @@ const Header = () => (
   <div id="header">
 
     <img id="profile_image" className="center" src={imgProfile} alt="profile" />
-    <Typography id="title" variant="display1">Alan David Ness</Typography>
-    <Typography id="subtext" variant="title">Full-Stack Web Developer</Typography>
 
-    <div id="header_icons" className="center">
-      <FontAwesomeIcon icon={faPhone} size="2x" />
-      <FontAwesomeIcon icon={faGithub} size="2x" />
-      <FontAwesomeIcon icon={faFilePdf} size="2x" />
-      <FontAwesomeIcon icon={faCodeBranch} size="2x" />
-      <FontAwesomeIcon icon={faEnvelope} size="2x" />
-      <FontAwesomeIcon icon={faCode} size="2x" />
-      <FontAwesomeIcon icon={faGithubAlt} size="2x" />
-    </div>
+    <span id="title">
+      <Typography variant="display1" gutterBottom>Alan David Ness</Typography>
+      <Typography variant="title" gutterBottom>Full-Stack Web Developer</Typography>
+      
+      <div id="header_icons" className="center">
+        <FontAwesomeIcon icon={faPhone} size="2x" />
+        <FontAwesomeIcon icon={faSlack} size="2x" />
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+        <FontAwesomeIcon icon={faFilePdf} size="2x" />
+        <FontAwesomeIcon icon={faEnvelope} size="2x" />
+      </div>
+    </span>
     
     <div id="header_bottom" className="center">
       <p>More info</p>
