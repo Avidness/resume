b2fbdebec6b0281622dcf997e025dddac037ec4b
Avidness <adavidness@gmail.com>
2018-07-19 18:44:46 -0500

    Extend customized muitheme. Move theme settings to index.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { Component } from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import React, { Fragment } from 'react';
 
 import Header from './components/Header'
 import Steps from './components/Steps'
 import './styles/App.css';
 
-const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-  },
-  typography: {
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      'Press Start 2P',
-    ].join(','),
-  },
-});
-
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
+      <Fragment>
         <Header />
         <Steps />
         <p className="App-intro parallax">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </MuiThemeProvider>
+      </Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      light: '#757ce8',
+      main: '#2c387e',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      'Press Start 2P',
+    ].join(','),
+  },
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>, 
+  document.getElementById('root'));
 registerServiceWorker();
