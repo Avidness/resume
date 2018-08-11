4321546ce480135f30c2d1dc7196501ca9a48a50
Avidness <adavidness@gmail.com>
2018-08-11 16:33:12 -0500

    Center content on the Contact page

--- src/components/Menu/Contact.js
+++ src/components/Menu/Contact.js
@@ -6,7 +6,7 @@ import { faPhone, faEnvelope } from '@fortawesome/fontawesome-free-solid';
 
 const Contact = () => {
   return (
-    <div className='parallax'>
+    <div className='parallax center'>
       <Typography variant='display1' paragraph={true}>
         Send me an email: 
       </Typography>
