b0e9f18285488e32fc813ff0534c2e63de94832a
Avidness <adavidness@gmail.com>
2018-08-10 23:26:53 -0500

    Cleanup app.js, remove unused component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import React, { Fragment } from 'react';
 
 import Header from './components/Header/';
 import Menu from './components/Menu/';
-import Steps from './components/Steps';
 import './styles/App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        <Header />
-        <Menu />
-        <Steps />
-        <p className="App-intro parallax">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </Fragment>
-    );
-  }
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Menu />
+    </Fragment>
+  )
 }
 
 export default App;
