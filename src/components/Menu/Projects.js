03a850975efbe099792fc2daa0d6bd740e1695f2
Avidness <adavidness@gmail.com>
2018-08-10 23:29:42 -0500

    Add content to the Projects page

--- src/components/Menu/Projects.js
+++ src/components/Menu/Projects.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 
 const Projects = () => {
   return (
-    <div>
-      projects
+    <div className='parallax'>
+      <Typography variant='display1' paragraph={true}>
+        Request for Access - Web app
+      </Typography>
+      <Typography variant='body2' paragraph={true}>
+        Website that centralizes RFA's from medical staff, volunteers, ancillary staff, and provides a control dashboard for the Provisioning team.
+      </Typography>
+
+      <Typography variant='display1'>
+        Survey Collection and Reporting
+      </Typography>
+      <Typography variant='body2' paragraph={true}>
+        Led a small team to develop multi-faced website that collects surveys and displays statistics to clients.
+      </Typography>
     </div>
   )
 }
