f4fc5ce68b245dcbf886a4b8844d8b8a9503a4af
Avidness <adavidness@gmail.com>
2018-08-11 17:40:16 -0500

    Update the skills list

--- src/components/Menu/Skills.js
+++ src/components/Menu/Skills.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import { List, Grid } from '@material-ui/core';
 import { faCode, faDatabase } from '@fortawesome/fontawesome-free-solid';
-import { faReact, faCss3Alt, faHtml5 } from '@fortawesome/fontawesome-free-brands';
+import { faReact, faCss3Alt, faHtml5, faGit, faAws, faAndroid } from '@fortawesome/fontawesome-free-brands';
 
 import SkillItem from './SkillItem';
 
 const Skills = () => {
   return (
-    <Grid container spacing={0}>
-      <Grid item xs={6}>
+    <Grid container spacing={0} className='parallax'>
+      <Grid item xs={12} md={6}>
         <List>
-          <SkillItem text="ReactJS" subtext="test2" progress={80} icon={faReact} />
-          <SkillItem text="CSS" subtext="test2" progress={90} icon={faCss3Alt} />
-          <SkillItem text="HTML" subtext="test2" progress={90} icon={faHtml5} />
-          <SkillItem text="C#" subtext="test2" progress={80} icon={faCode} />
-          <SkillItem text="SQL" subtext="test2" progress={70} icon={faDatabase} />
+          <SkillItem text="C#" progress={95} icon={faCode} />
+          <SkillItem text="ReactJS" progress={80} icon={faReact} />
+          <SkillItem text="AWS" progress={70} icon={faAws} />
+          <SkillItem text=".NET" progress={90} icon={faCode} />
+          <SkillItem text="dotnet core" progress={80} icon={faCode} />
+          <SkillItem text="EF core" progress={70} icon={faDatabase} />
+          <SkillItem text="NHibernate" progress={80} icon={faDatabase} />
+          <SkillItem text="Redux" progress={70} icon={faCode} />
         </List>
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={12} md={6}>
         <List>
-          <SkillItem text="ReactJS" subtext="test2" progress={80} icon={faReact} />
-          <SkillItem text="CSS" subtext="test2" progress={90} icon={faCss3Alt} />
-          <SkillItem text="HTML" subtext="test2" progress={90} icon={faHtml5} />
-          <SkillItem text="C#" subtext="test2" progress={80} icon={faCode} />
-          <SkillItem text="SQL" subtext="test2" progress={70} icon={faDatabase} />
+          <SkillItem text="SQL Server" progress={95} icon={faDatabase} />
+          <SkillItem text="Git" progress={90} icon={faGit} />
+          <SkillItem text="Restful APIs" progress={90} icon={faCode} />
+          <SkillItem text="CSS" progress={95} icon={faCss3Alt} />
+          <SkillItem text="HTML" progress={95} icon={faHtml5} />
+          <SkillItem text="MVC5" progress={95} icon={faCode} />
+          <SkillItem text="Android" progress={50} icon={faAndroid} />
         </List>
       </Grid>
     </Grid>
