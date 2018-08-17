7ef6995ee12885c049e57951350597160b0288f1
Alan Ness <userbon@scimitar.attlocal.net>
2018-08-17 15:13:02 -0400

    Attempt to fix Netlify link by using target=self

--- src/components/Header/IconLink.js
+++ src/components/Header/IconLink.js
@@ -4,7 +4,7 @@ import FaIcon from '@fortawesome/react-fontawesome';
 
 const IconLink = (props) => (
   <Tooltip title={props.tooltip}>
-    <a href={props.href} target="blank">
+    <a href={props.href} target="_self">
       <Grow in={true} timeout={2500}>
         <FaIcon icon={props.icon} size="2x" />
       </Grow>
