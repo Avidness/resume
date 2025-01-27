fe5615e726db28a0f19108a6ca9fd7fc18fa6b24
avidness <adavidness@gmail.com>
2025-01-27 15:19:37 -0500

    fix outdated fontawesome imports

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
-import FaIcon from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope } from '@fortawesome/fontawesome-free-solid';
 import SlideWrapper from '../SlideWrapper/';
 
@@ -13,7 +13,7 @@ const Contact = () => {
         </Typography>
         <Typography variant='headline' paragraph={true}>
           <a target='blank' href='mailto:adavidness@gmail.com'>
-            <FaIcon icon={faEnvelope} /> adavidness@gmail.com 
+            <FontAwesomeIcon icon={faEnvelope} /> adavidness@gmail.com 
           </a>
         </Typography>
     </SlideWrapper>
@@ -24,7 +24,7 @@ const Contact = () => {
       </Typography>
       <Typography variant='headline' paragraph={true}>
         <a target='blank' href='tel:4077339763'>
-          <FaIcon icon={faPhone} /> 1-407-733-9763
+          <FontAwesomeIcon icon={faPhone} /> 1-407-733-9763
         </a>
       </Typography>
     </SlideWrapper>
--- src/components/Header/IconLink.js
+++ src/components/Header/IconLink.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Tooltip, Grow } from '@material-ui/core';
-import FaIcon from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const IconLink = (props) => (
   <Tooltip title={props.tooltip}>
     <a href={props.href} target="blank">
       <Grow in={true} timeout={2500}>
-        <FaIcon icon={props.icon} size="2x" />
+        <FontAwesomeIcon icon={props.icon} size="2x" />
       </Grow>
     </a>
   </Tooltip>
